2b30f4dd6a24e7def203c159d481352d863fcc37
Maria <machnevamaria@gmail.com>
2024-09-24 18:31:08 +0300

    Дорабатывает задачи по замечаниям наставника

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,13 @@
-const CheckRandomString = (string, maxLength) => {
-  const check = string.length <= maxLength;
-  return check;
+const сheckRandomString = (string, maxLength) => string.length <= maxLength;
+сheckRandomString('266af55', 5);
+
+const palindromeString = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  let reversedString = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return string === reversedString;
 };
 
-CheckRandomString('266af55', 5);
-
-function PalindromeString(string) {
-  string = string.toLowerCase().replaceAll(' ', '');
-  const lastIndex = string.length - 1;
-  for (let i = lastIndex; i >= 0; i--) {
-    if (string.at(i) === string.at(lastIndex - i)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-PalindromeString('Лёша на полке клопа нашёл ');
+palindromeString('ствооооооооооооооол');
