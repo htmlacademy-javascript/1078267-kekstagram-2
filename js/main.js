7565a76840230015640698d0605897cc0bedf7a3
Maria <machnevamaria@gmail.com>
2024-10-23 22:00:34 +0300

    Добавляет скобки в условие функции-генератора id комментария

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ function createRandomIdFromRangeGenerator(min, max) {
   const previousValues = [];
   return function () {
     let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= max - min + 1) {
+    if (previousValues.length >= (max - min + 1)) {
       // console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
       return null;
     }
