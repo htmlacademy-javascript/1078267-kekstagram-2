165d36778a6a7eaec96a0e540ccc1cc211685c31
Maria <machnevamaria@gmail.com>
2024-10-23 21:04:09 +0300

    Дорабатывает функцию генератор ID комментария

--- js/main.js
+++ js/main.js
@@ -69,6 +69,10 @@ function createRandomIdFromRangeGenerator(min, max) {
   const previousValues = [];
   return function () {
     let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      // console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+      return null;
+    }
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
     }
