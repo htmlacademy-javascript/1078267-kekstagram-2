e994f1846f66f8f687fa70b21cc73957292a71de
Maria <machnevamaria@gmail.com>
2024-11-25 21:09:04 +0300

    отрисовывает окно с полноразмерным изображением

--- js/big-photos.js
+++ js/big-photos.js
@@ -1,7 +1,9 @@
-import { MOCKED_PHOTOS} from './examples.js';
+import { MOCKED_PHOTOS } from './examples.js';
 
 const bigPicture = document.querySelector('.big-picture');
-const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
+const bigPictureImg = bigPicture
+  .querySelector('.big-picture__img')
+  .querySelector('img');
 const likesCount = bigPicture.querySelector('.likes-count');
 const socialComments = bigPicture.querySelector('.social__comments');
 const socialCommentTemplate = socialComments.querySelector('.social__comment');
@@ -9,7 +11,6 @@ const commentsCaption = bigPicture.querySelector('.social__caption');
 const commentsCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.social__comments-loader');
 const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
-
 
 const onBigPictureCancelClick = () => {
   closeBigPicture();
@@ -26,5 +27,34 @@ const closeBigPicture = () => {
 };
 
 const openBigPicture = (pictureId) => {
-const currentPhoto = MOCKED_PHOTOS.find((photo) => photo.id === Number(pictureId));
+  const currentPhoto = MOCKED_PHOTOS.find(
+    (photo) => photo.id === Number(pictureId)
+  );
+  const socialCommentsFragment = document.createDocumentFragment();
+
+  bigPictureImg.src = currentPhoto.url;
+  likesCount.textContent = currentPhoto.likes;
+  socialComments.innerHTML = '';
+
+  currentPhoto.comments.forEach((comment) => {
+    const socialComment = socialCommentTemplate.cloneNode(true);
+    socialComment.querySelector('social__picture').src = comment.avatar;
+    socialComment.querySelector('social__picture').alt = comment.name;
+    socialComment.querySelector('social__picture').textContent =
+      comment.message;
+
+    socialCommentsFragment.appendChild(socialComment);
+  });
+
+  socialComments.appendChild(socialCommentsFragment);
+  commentsCaption.textContent = currentPhoto.description;
+  commentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+
+  bigPicture.classList.remove('hidden');
+  bigPictureCancel.addEventListener('click', onBigPictureCancelClick);
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onEscKeydown);
 };
+
+export { openBigPicture };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 import { MOCKED_PHOTOS} from './examples.js';
 import { renderThumbnail} from './thumbnails.js';
+import { openBigPicture} from './big-photos.js';
 renderThumbnail(MOCKED_PHOTOS);
+
+ctrateThumbnail(MOCKED_PHOTOS).addEventListener('click', (evt) => {
+  const currentPicture = evt.target.closest('.picture');
+
+  if (currentPicture) {
+    openBigPicture(openBigPicture.dataset.pictureID);
+  }
+});
