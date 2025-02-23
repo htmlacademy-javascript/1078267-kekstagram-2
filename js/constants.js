41496c103fe106d7104bc11e1f4de69514081b01
Maria <machnevamaria@gmail.com>
2025-02-23 20:14:33 +0300

    Исправляет нейминг объектов и функций

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-const FILTER = {
+const Filter = {
   default: 'filter-default',
   random: 'filter-random',
   discussed: 'filter-discussed',
@@ -13,4 +13,4 @@ const MAX_PICTURE_COUNT = 10;
 
 const FILE_TYPES = ['jpg', 'jpeg', 'png', 'gif', 'jfif'];
 
-export { FILTER, SortFunc, MAX_PICTURE_COUNT, FILE_TYPES };
+export { Filter, SortFunc, MAX_PICTURE_COUNT, FILE_TYPES };
