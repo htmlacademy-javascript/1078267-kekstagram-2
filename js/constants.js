ed65545dbc779d8de76c5c9e7a0f6513f310ae2f
Maria <machnevamaria@gmail.com>
2025-02-26 22:49:51 +0300

    Дорабатывает наименования констант

--- js/constants.js
+++ js/constants.js
@@ -5,8 +5,8 @@ const Filter = {
 };
 
 const SortFunc = {
-  GET_RANDOM_PREVIEW: () => 0.5 - Math.random(),
-  GET_DISCUSSED_PREVIEW: (a, b) => b.comments.length - a.comments.length,
+  GET_RANDOM_PREVIEWS: () => 0.5 - Math.random(),
+  GET_DISCUSSED_PREVIEWS: (a, b) => b.comments.length - a.comments.length,
 };
 
 const MAX_PICTURE_COUNT = 10;
--- js/filter.js
+++ js/filter.js
@@ -4,6 +4,7 @@ import { debounce } from './utils.js';
 import { Filter, SortFunc, MAX_PICTURE_COUNT } from './constants.js';
 
 const ACTIVE_BUTTON_CLASS = 'img-filters__button--active';
+
 let currentFilter = 'filter-default';
 let photos = [];
 const filterElement = document.querySelector('.img-filters');
@@ -35,10 +36,10 @@ function applyFilter() {
     filteredPictures = photos;
   }
   if (currentFilter === Filter.RANDOM) {
-    filteredPictures = photos.toSorted(SortFunc.GET_RANDOM_PREVIEW).slice(0, MAX_PICTURE_COUNT);
+    filteredPictures = photos.toSorted(SortFunc.GET_RANDOM_PREVIEWS).slice(0, MAX_PICTURE_COUNT);
   }
   if (currentFilter === Filter.DISCUSSED) {
-    filteredPictures = photos.toSorted(SortFunc.GET_DISCUSSED_PREVIEW);
+    filteredPictures = photos.toSorted(SortFunc.GET_DISCUSSED_PREVIEWS);
   }
   debounceRender(filteredPictures);
 }
